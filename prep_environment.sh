d150e733561a217618849eafda725d6adf14f621
Mark Ravinet <markravinet@1x-193-157-247-6.uio.no>
2017-11-14 12:52:18 +0100

    Altered prep environ

--- prep_environment.sh
+++ prep_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# common variables
+
+# variables needed for the workshop
 export DATA_DIR=/projects/cees2/in_progress/cees_workshop
 export REF=/projects/cees2/in_progress/cees_workshop/ref/house_sparrow_genome_assembly-18-11-14_masked.fa
-# set up CEES stuff
-export PATH=/projects/cees/bin:/projects/cees/scripts:$PATH
-umask 0002
-module use --append /projects/cees/bin/modules
+export PICARD_PATH=/projects/cees/bin/picard/2.7.1/picard.jar
+export GATK_PATH=/cluster/software/VERSIONS/gatk-3.7/GenomeAnalysisTK.jar
+
